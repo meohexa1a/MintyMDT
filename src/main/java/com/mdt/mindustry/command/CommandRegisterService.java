b86b18f8163140ff701271c3a43e06178415251e
meohexa1a <meohexa.1.a@gmail.com>
2026-01-18 20:35:18 +0700

    maybe meow

--- src/main/java/com/mdt/mindustry/command/CommandRegisterService.java
+++ src/main/java/com/mdt/mindustry/command/CommandRegisterService.java
@@ -7,8 +7,10 @@ import mindustry.gen.Player;
 import lombok.Locked;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
+
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
@@ -24,11 +26,11 @@ public final class CommandRegisterService {
     // !---------------------------------------------------------------------!
 
     @Locked
-    public void registerCommands(String group,
+    public void registerCommands(
+            String group,
             @NotNull Set<ClientCommand> clientCommands,
             @NotNull Set<ConsoleCommand> consoleCommands) {
-        if (registered.containsKey(group))
-            unregister(group);
+        if (registered.containsKey(group)) unregister(group);
 
         registered.put(group, new Pair<>(clientCommands, consoleCommands));
         clientCommands.forEach(this::registerClient);
@@ -38,27 +40,25 @@ public final class CommandRegisterService {
     @Locked
     public void unregister(String group) {
         var pair = registered.remove(group);
-        if (pair == null)
-            return;
+        if (pair == null) return;
 
-        pair.first().forEach(
-                clientCommand -> clientCommand.prefixes().forEach(MintyMDTPlugin.getClientHandler()::removeCommand));
-        pair.second().forEach(
-                consoleCommand -> consoleCommand.prefixes().forEach(MintyMDTPlugin.getServerHandler()::removeCommand));
+        pair.first().forEach(clientCommand ->
+                clientCommand.prefixes().forEach(MintyMDTPlugin.getClientHandler()::removeCommand));
+        pair.second().forEach(consoleCommand ->
+                consoleCommand.prefixes().forEach(MintyMDTPlugin.getServerHandler()::removeCommand));
     }
 
     // !--------------------------------------------------------!
 
     private void registerClient(ClientCommand cmd) {
         for (var prefix : cmd.prefixes())
-            MintyMDTPlugin.getClientHandler().<Player>register(prefix, cmd.args(), cmd.description(),
-                    (args, player) -> {
-                        try {
-                            cmd.action().accept(args, player);
-                        } catch (Exception e) {
-                            log.error("Error while executing command | {}", prefix, e);
-                        }
-                    });
+            MintyMDTPlugin.getClientHandler().<Player>register(prefix, cmd.args(), cmd.description(), (args, player) -> {
+                try {
+                    cmd.action().accept(args, player);
+                } catch (Exception e) {
+                    log.error("Error while executing command | {}", prefix, e);
+                }
+            });
     }
 
     private void registerConsole(ConsoleCommand cmd) {
