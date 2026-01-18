efaa154ec45bb2529012da47dd492f8e89eb16df
meohexa1a <meohexa.1.a@gmail.com>
2026-01-18 14:52:56 +0700

    maybe meow

--- src/main/java/com/mdt/MintyMDTPlugin.java
+++ src/main/java/com/mdt/MintyMDTPlugin.java
@@ -1,18 +1,23 @@
 package com.mdt;
 
-import mindustry.mod.Plugin;
-import org.codejargon.feather.Feather;
-
+import com.mdt.di.Prototype;
 import arc.util.CommandHandler;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import mindustry.mod.Plugin;
+import org.codejargon.feather.Feather;
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
+
+import javax.inject.Singleton;
+import java.util.HashSet;
+import java.util.Set;
 
 @Slf4j
 public final class MintyMDTPlugin extends Plugin {
     private static @Getter CommandHandler serverHandler, clientHandler = null;
+    private static @Getter Feather feather;
     private volatile boolean isStarted = false;
-
-    // !----------------------------------------------------------------!
 
     @Override
     public void registerServerCommands(CommandHandler handler) {
@@ -30,6 +35,27 @@ public final class MintyMDTPlugin extends Plugin {
         if (isStarted || serverHandler == null || clientHandler == null) return;
         this.isStarted = true;
 
-        
+        try {
+            MintyMDTPlugin.feather = Feather.with(new HashSet<>() {{
+                addAll(scanAnnotatedClasses());
+
+                add(serverHandler);
+                add(clientHandler);
+            }});
+        } catch (Exception e) {
+            log.error("Failed to initialize Dependency Injection. Exit application", e);
+            System.exit(-1);
+        }
+
+        log.info("Successfully initialized");
+    }
+
+    private Set<Class<?>> scanAnnotatedClasses() {
+        return new HashSet<>() {{
+            var reflections = new Reflections("com.mdt", Scanners.TypesAnnotated);
+
+            addAll(reflections.getTypesAnnotatedWith(Prototype.class));
+            addAll(reflections.getTypesAnnotatedWith(Singleton.class));
+        }};
     }
 }
