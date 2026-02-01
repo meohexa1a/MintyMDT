50df1ada2bed5fcea4dab03984fbd1e35d8c05d4
meohexa1a <meohexa.1.a@gmail.com>
2026-02-01 11:39:24 +0700

    maybe meow

--- src/main/java/com/mdt/mindustry/command/ClientCommand.java
+++ src/main/java/com/mdt/mindustry/command/ClientCommand.java
@@ -1,11 +1,13 @@
 package com.mdt.mindustry.command;
 
 import lombok.Builder;
+import lombok.Generated;
 import lombok.NonNull;
 import lombok.Singular;
 
 import mindustry.gen.Player;
 
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.function.BiConsumer;
 
@@ -16,4 +18,8 @@ public record ClientCommand(
     @NonNull String args,
     @NonNull BiConsumer<String[], Player> action) {
 
+    public static class ClientCommandBuilder {
+        private @Generated String description = "";
+        private @Generated String args = "";
+    }
 }
